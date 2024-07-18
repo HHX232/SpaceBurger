78d7ec3d3ae69f821228906d225deda3330ffc46
HHX232 <140748811+HHX232@users.noreply.github.com>
2024-07-18 16:39:30 +0300

    header create

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AppHeader from './components/AppHeader/AppHeader';
+
+function App() {
+  return (
+    <AppHeader />
+  );
+}
+
+export default App;
---
+++ src/components/AppHeader/AppHeader.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { BurgerIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { ListIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Logo } from '@ya.praktikum/react-developer-burger-ui-components';
+import { ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+
+import stylesHeader from './AppHeader.module.css';
+
+function NavItem(props){
+    return(
+        <li className={`${stylesHeader.nav__item} pt-4 pb-4 pl-5 pr-5`}>
+        {props.icon}
+        <a className={props.textType} href={props.href}>{props.text}</a>
+        </li>
+    )
+}
+
+
+class ProfileButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isActive: false,
+    };
+    this.handleDoubleClick = this.handleDoubleClick.bind(this);
+  }
+  handleDoubleClick() {
+    this.setState((prevState) => ({
+      isActive: !prevState.isActive,
+    }));
+  }
+  render() {
+    const { isActive } = this.state;
+    const buttonText = isActive ? 'Войти' : 'Личный кабинет';
+    const buttonLink = isActive ? '#' : '#';
+
+    return (
+      <a
+        href={buttonLink}
+        className={`${stylesHeader.profile__box} pt-4 pb-4 pl-5 pr-5`}
+        onDoubleClick={this.handleDoubleClick}
+      >
+        <ProfileIcon type='secondary'/>
+        <span className="text text_type_main-default text__disactive">{buttonText}</span>
+      </a>
+    );
+  }
+}
+
+
+
+function AppHeader(){
+    return (
+        <nav className={`${stylesHeader.header} pt-4 pb-4`} >
+            <div className={stylesHeader.header__container}>
+            <div className={stylesHeader.header__inner}>
+            <ul className={stylesHeader.nav__list}>
+            <NavItem textType="text text_type_main-default text__active" icon={<BurgerIcon />}  text="Конструктор" href="#"/>
+            <NavItem textType="text text_type_main-default text__disactive" 
+            icon={<ListIcon type='secondary'/>}  text="Лента заказов" href="#"/>
+            </ul>
+            <div className={stylesHeader.header__logo}>
+            <Logo  />
+            </div>
+            <ProfileButton />
+            </div>
+            </div>
+        </nav>
+    )
+}
+
+export default AppHeader;
